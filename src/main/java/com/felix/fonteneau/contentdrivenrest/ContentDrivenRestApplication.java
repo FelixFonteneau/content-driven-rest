96e5bdba225bd2557b0678f19893a33b2990682d
felixfon <felix.fonteneau@hotmail.com>
2021-11-04 17:06:42 +0100

    init

---
+++ src/main/java/com/felix/fonteneau/contentdrivenrest/ContentDrivenRestApplication.java
@@ -0,0 +1,13 @@
+package com.felix.fonteneau.contentdrivenrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentDrivenRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentDrivenRestApplication.class, args);
+    }
+
+}
